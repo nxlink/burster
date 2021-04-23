e3a8e1a7528172cdae0cffcc3a5fee6d506e3488
Bradley Shaw <brad@team.nxlik.com>
2021-04-23 10:58:05 -0500

    burster updating from database

--- burster.py
+++ burster.py
@@ -7,6 +7,7 @@ import argparse
 import datetime
 import MySQLdb as mdb
 import MySQLdb.cursors
+import sys
 
 def ReadCSVFile(filename):
 	rowdictlist = []
@@ -21,8 +22,8 @@ def CalcMTRateLimit(row,perc,main_config):
 	dl_rate = {}
 	sbp = float(main_config['sbp'])
 	burst_period = float(main_config['burst_period'])
-	ul_rate['base'] = float(row['UL']) * 1000
-	dl_rate['base'] = float(row['DL']) * 1000
+	ul_rate['base'] = float(row['UL']) * 1000 * (1 + (float(main_config["boost_perc"]) / 100))
+	dl_rate['base'] = float(row['DL']) * 1000 * (1 + (float(main_config["boost_perc"]) / 100))
 	if (int(perc) >= 100):
 		ul_rate['max'] = int(ul_rate['base'] * (float(perc) / 100))
 		dl_rate['max'] = int(dl_rate['base'] * (float(perc) / 100))
@@ -57,23 +58,111 @@ def UpdateRADDB(row,perc,raddb_config,main_config):
 			con.commit()
 			con.close()
 
+
+def read_plan_table(config):
+	bbdb_creds = dict(config.items("bbdb"))
+	try:
+		con = mdb.connect(host=bbdb_creds["host"],database=bbdb_creds["db"],user=bbdb_creds["user"],password=bbdb_creds["pass"]);
+		cur = con.cursor(MySQLdb.cursors.DictCursor)
+		cur.execute("""SELECT * FROM plans;""")
+		rows = cur.fetchall()
+		return rows
+	except mdb.Error, e:
+		print "Error %d: %s" % (e.args[0],e.args[1])
+		sys.exit(1)
+	finally:
+		if con:
+			con.commit()
+			con.close()
+
+def create_temp_tables(config):
+	raddb_creds = dict(config.items("raddb"))
+	try:
+		con = mdb.connect(host=raddb_creds["host"],database=raddb_creds["db"],user=raddb_creds["user"],password=raddb_creds["pass"]);
+		cur = con.cursor()
+		cur.execute("""DROP table IF EXISTS radgroupcheck_tmp;""")
+		con.commit()
+		cur.execute("""DROP table IF EXISTS radgroupreply_tmp;""")
+		con.commit()
+		cur.execute("""CREATE table radgroupcheck_tmp LIKE radgroupcheck_template;""")
+		con.commit()
+		cur.execute("""CREATE table radgroupreply_tmp LIKE radgroupreply_template;""")
+		con.commit()
+	except mdb.Error, e:
+		print "Error %d: %s" % (e.args[0],e.args[1])
+		sys.exit(1)
+	finally:
+		if con:
+			con.commit()
+			con.close()
+
+def swap_temp_tables(config):
+	raddb_creds = dict(config.items("raddb"))
+	try:
+		con = mdb.connect(host=raddb_creds["host"],database=raddb_creds["db"],user=raddb_creds["user"],password=raddb_creds["pass"]);
+		cur = con.cursor()
+		cur.execute("""DROP table radgroupcheck;""")
+		con.commit()
+		cur.execute("""DROP table radgroupreply;""")
+		con.commit()
+		cur.execute("""RENAME table radgroupcheck_tmp to radgroupcheck;""")
+		con.commit()
+		cur.execute("""RENAME table radgroupreply_tmp to radgroupreply;""")
+		con.commit()
+	except mdb.Error, e:
+		print "Error %d: %s" % (e.args[0],e.args[1])
+		sys.exit(1)
+	finally:
+		if con:
+			con.commit()
+			con.close()
+
+def update_temp_tables(config,row,perc):
+	raddb_creds = dict(config.items("raddb"))
+	main_config = dict(config.items("main"))
+	mt_rate_limit_str = CalcMTRateLimit(row,perc,main_config)
+
+	try:
+		con = mdb.connect(host=raddb_creds["host"],database=raddb_creds["db"],user=raddb_creds["user"],password=raddb_creds["pass"]);
+		cur = con.cursor()
+		cur.execute("""INSERT into radgroupcheck_tmp (groupname,attribute,op,value) VALUES ('{groupname}','{attribute}','{radop}','{radvalue}');""".format(groupname=row['PLAN'],attribute="Auth-Type",radop=":=",radvalue="Local"))
+		cur.execute("""INSERT into radgroupreply_tmp (groupname,attribute,op,value) VALUES ('{groupname}','{attribute}','{radop}','{radvalue}');""".format(groupname=row['PLAN'],attribute="Session-Timeout",radop=":=",radvalue=main_config["session_timeout"]))
+		cur.execute("""INSERT into radgroupreply_tmp (groupname,attribute,op,value) VALUES ('{groupname}','{attribute}','{radop}','{radvalue}');""".format(groupname=row['PLAN'],attribute="Framed-Pool",radop=":=",radvalue=main_config["framed_pool"]))
+		cur.execute("""INSERT into radgroupreply_tmp (groupname,attribute,op,value) VALUES ('{groupname}','{attribute}','{radop}','{radvalue}');""".format(groupname=row['PLAN'],attribute="Mikrotik-Rate-Limit",radop=":=",radvalue=mt_rate_limit_str))
+		con.commit()
+	except mdb.Error, e:
+		print "Error %d: %s" % (e.args[0],e.args[1])
+		sys.exit(1)
+	finally:
+		if con:
+			con.commit()
+			con.close()
+
+
+
 def main():
 	rowdictlist = []
 	config = RawConfigParser()
 	config.read('/opt/burster/burster.cfg')
 	parser = argparse.ArgumentParser()
-	parser.add_argument('-f',"--file", help="Plans CSV file")
+	#parser.add_argument('-f',"--file", help="Plans CSV file")
 	parser.add_argument('-p',"--percent", help="Burst percent")
 	args = parser.parse_args()
 	perc = args.percent
-	rowdictlist = ReadCSVFile(args.file)
-	raddb_config = dict(config.items("raddb"))
-	main_config = dict(config.items("main"))
-	print perc
+	#rowdictlist = ReadCSVFile(args.file)
+	rowdictlist = read_plan_table(config)
+	print rowdictlist
+	create_temp_tables(config)
 	for row in rowdictlist:
-		UpdateRADDB(row,perc,raddb_config,main_config)
-		calcstr = CalcMTRateLimit(row,perc,main_config)
-		print "%s,%s" % (row['PLAN'],calcstr)
+		update_temp_tables(config,row,perc)
+	swap_temp_tables(config)
+	# raddb_config = dict(config.items("raddb"))
+	# main_config = dict(config.items("main"))
+	# print perc
+	# for row in rowdictlist:
+	# 	UpdateRADDB(row,perc,raddb_config,main_config)
+	# 	calcstr = CalcMTRateLimit(row,perc,main_config)
+	# 	print "%s,%s" % (row['PLAN'],calcstr)
 
 
 
